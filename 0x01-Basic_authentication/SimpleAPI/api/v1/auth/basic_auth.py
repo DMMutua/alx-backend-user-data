948753aa63cbfd3b7ea0b03ac56a4f405e23a051
DMMutua <lengowriter99@gmail.com>
2023-05-29 18:48:50 +0300

    basic_auth class

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Basic Authentication Implementation"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """A Class that Implements Basic Authentication"""
+    pass
