e56e0dd252a83ea61ad396a05d7bdedc5ea410bd
DMMutua <lengowriter99@gmail.com>
2023-05-29 16:12:53 +0300

    updating `Auth` to check whether auth is required or not

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -13,8 +13,27 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns False for Now"""
-        return False
+        """Method that checks whether a certain `path` is accessible.
+        Leverages a List of `excluded_paths` for special cases.
+        Returns:
+            `True` if auth is required.`False` if otherwise.
+        """
+        
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        
+        if path is None:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if path == excluded_path:
+                return False
+        
+        return True
+
 
     def authorization_header(self, request=None) -> str:
         """Returns `None` for Now"""
