b148ac7ad2d2fa4d4f2a6092d79b673e315e8932
DMMutua <lengowriter99@gmail.com>
2023-05-30 20:02:19 +0300

    returning User instance based on the email and password processed with asic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 
 import base64
 from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -57,3 +59,25 @@ class BasicAuth(Auth):
         email, password = decoded_base64_authorization_header.split(':', 1)
 
         return email, password
+
+    def user_object_from_credentials(self,
+                                     user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """Return the User Instance based on their Email and Password"""
+        if not user_email or not user_pwd:
+            return None
+
+        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({'email': user_email})
+
+        if not users:
+            return None
+
+        user = users[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
