8a8d58143f03e167ff3f39e97f3c8274d3ed00e0
DMMutua <lengowriter99@gmail.com>
2023-06-08 12:57:14 +0300

    creating a new Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,19 +2,46 @@
 
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
-def _hash_password(password: str) -> bytes:
-    """Generates a salted hash of the input password using bcrypt.
+class Auth:
+    """An Authentication Class for DB class"""
 
-    Args:
-        password (str): The password to hash.
+    def __init__(self):
+        self._db = DB()
 
-    Returns:
-        bytes: The salted hash of the password.
-    """
-    salt = bcrypt.gensalt()
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a new user and returns the User object.
+        Returns User Object
+        Raises:
+            ValueError: If a user already exists with the passed email.
+        """
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            if existing_user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
 
-    hashed_pw = bcrypt.hashpw(password.encode('utf-8'), salt)
+        hashed_pw = self._hash_password(password)
+        user = self._db.add_user(email=email,
+                                  hashed_password=hashed_pw)
+        return user
 
-    return hashed_pw
+    @staticmethod
+    def _hash_password(password: str) -> bytes:
+        """Generates a salted hash of the input password using bcrypt.
+
+        Args:
+            password (str): The password to hash.
+
+        Returns:
+            bytes: The salted hash of the password.
+        """
+        salt = bcrypt.gensalt()
+        hashed_pw = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+        return hashed_pw
