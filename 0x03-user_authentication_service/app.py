85266dd8eec74218451f5e9bd876ce055f91a8e3
DMMutua <lengowriter99@gmail.com>
2023-06-08 17:30:18 +0300

    an endpoint in the app for login and authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,6 +3,7 @@
 
 
 from flask import Flask, jsonify, request
+from flask import make_response, abort
 from auth import Auth
 
 
@@ -28,6 +29,32 @@ def users():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """Get Email and Password from Request Form Data"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        abort(401)
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+
+    response_payload = {
+        "email": email,
+        "message": "logged in"
+    }
+
+    response = make_response(jsonify(response_payload))
+
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
