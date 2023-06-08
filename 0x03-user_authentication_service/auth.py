d5f27dc465c4a8dd7deda1de31b1266a78d7ddd1
DMMutua <lengowriter99@gmail.com>
2023-06-08 16:33:07 +0300

    a method to verify credentials of a login attempt

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,3 +45,23 @@ class Auth:
         hashed_pw = bcrypt.hashpw(password.encode('utf-8'), salt)
 
         return hashed_pw
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validates a login attempt.
+
+        Args:
+            email (str): The email of the user.
+            password (str): The password of the user.
+
+        Returns:
+            bool: True if the login is valid, False otherwise.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user and bcrypt.checkpw(password.encode('utf-8'),
+                                       user.hashed_password):
+                return True
+        except NoResultFound:
+            pass
+
+        return False
